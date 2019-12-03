7d7c754485d62534caf32363fe96de74d58bb5b5
natya2203 <nastya_2203@ukr.net>
2019-12-03 16:17:51 +0200

    solution

---
+++ src/components/Header/Header.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Header.css';
+
+function Header() {
+  return (
+    <>
+      <section className="welcome">
+        <span className="welcome__text">Sticky Header!</span>
+      </section>
+      <header className="header">
+        <h1 className="header__title">Site Name</h1>
+        <nav className="navigation">
+          <a className="navigation__link" href="#">About</a>
+          <a className="navigation__link" href="#">Services</a>
+          <a className="navigation__link" href="#">Contact</a>
+        </nav>
+      </header>
+    </>
+  );
+}
+
+export default Header;
